d5b41f360aa08b69bf3a9feac4a876fb8cce5ed5
Adam Wright <adamtwright7@gmail.com>
2023-03-04 17:42:54 -0500

    Finishing touches for live site.
    Some finishing touches for the live site.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3010;
 // importing stuff for sessions and cookies
 app.use(express.static(__dirname + "/public"));
 const session = require("express-session"); // for sessions
